a3e220259666ffdf0e0a5f044aeb5bf05c941080
Gabriela Hilario <84292765+gabriela189816@users.noreply.github.com>
2022-11-11 22:00:56 -0600

    Última versión estable de 3Dinv_pendulum.py

--- 3Dinv_pendulum.py
+++ 3Dinv_pendulum.py
@@ -11,23 +11,19 @@ from vpython import *
 import numpy as np 
 
 # Configuración de la Escena
-scene.width = 700
-scene.height = 400
+scene.width = 600
+scene.height = 300
 scene.background = color.white
-scene.title = "Animación 3D del carrito con péndulo"
-# Ejes coordenados
-ejex = arrow(pos=vector(-2,-1.5,0), size=vector(10,0.1,0.1), shaftwidth=0.05, color=color.black, visible=True)
-ejey = arrow(pos=vector(-2,-1.5,0), size=vector(7,0.1,0.1), shaftwidth=0.05, color=color.black, visible=True)
-ejey.rotate(angle=pi/2, axis=vec(0,0,1))
-p = sphere(pos=vector(0,-1.5,0), color=color.black, radius=0.1)
-p = sphere(pos=vector(1,-1.5,0), color=color.black, radius=0.1)
-p = sphere(pos=vector(2,-1.5,0), color=color.black, radius=0.1)
-p = sphere(pos=vector(3,-1.5,0), color=color.black, radius=0.1)
-p = sphere(pos=vector(4,-1.5,0), color=color.black, radius=0.1)
-p = sphere(pos=vector(5,-1.5,0), color=color.black, radius=0.1)
-p = sphere(pos=vector(6,-1.5,0), color=color.black, radius=0.1)
-p = sphere(pos=vector(7,-1.5,0), color=color.black, radius=0.1)
-p = sphere(pos=vector(8,-1.5,0), color=color.black, radius=0.1)
+scene.title = "\t \t \t Animación 3D del carrito con péndulo \n \t \t \t \t Con f = 1"
+# Ejes coordenados y etiquetas
+ejex = arrow(pos=vector(0,-1.5,0), size=vector(10,0.1,0.1), shaftwidth=0.05, color=color.black, visible=True)
+ejex2 = arrow(pos=vector(0,-1.5,0), size=vector(10,0.1,0.1), shaftwidth=0.05, color=color.black, visible=True)
+ejex2.rotate(angle=pi, axis=vec(0,0,1))
+o = cylinder(pos=vector(0,-2,0), size=vector(0.5,0.1,0.1),color=color.black)
+o.rotate(angle=pi/2, axis=vec(0,0,1))
+t0 = label(text='0', pos = vector(0,-2.5,0))
+t2 = label(text='x', pos = vector(10,-2.5,0))
+t3 = label(text='-x', pos = vector(-10,-2.5,0))
 
 # PARÁMETROS
 l = 4           # longitud de la varilla
@@ -42,7 +38,7 @@ xdot = 0        # dx/dt (velocidad lineal) inicial
 thdot = 0       # dth/dt (velocidad angular) inical
 # tiempo y paso de tiempo
 t = 0           # tiempo inicial
-dt = 0.02      # paso de tiempo
+dt = 0.02       # paso de integración
 
 # Coordenadas y velocidades generalizadas
 q = np.array([[x],[th]])
@@ -50,40 +46,22 @@ qdot = np.array([[xdot], [thdot]])
 print(f'Posiciones iniciales: \n x = {q[0]}, theta = {q[1]} radianes')
 print(f'Velocidades iniciales: \n xdot = {qdot[0]}, thetadot = {qdot[1]}')
 
-# Matriz de Incercia H(q)
-H_list = [[mc+mp, mp*l*cos(th)],[mp*l*cos(th), mp*l**2]]
-H = np.array(H_list)
-print(f'\n Matriz de Inercia H(q) = {H}')
-HI= np.linalg.inv(H) #Inversa de H
-# Matriz de Coriolis
-C_list = [[0, -thdot*mp*l*sin(th)],[0,0]]
-C = np.array(C_list)
-print(f'\n Matriz de Corilis C(q,qdot) = {C}')
-# Vector de Gravedad
-G = np.array([[0],[mp*l*g*sin(th)]])
-print(f'Vector de Gravedad G(q) = {G}')
-# Vector Bu
-B = np.array([[1],[0]])
-Bu = np.dot(B,f)
-#print(f'Vector de Bu = {Bu}')
-
 # ------------------------------------ Animación 3D ------------------------------
 # posición inical del carito
-car = box(pos=vector(x,0,0), size=vector(4,2,2), color=color.cyan, visible=True)
-s1 = sphere(pos=vector(-1.5,-1,1), color=color.black, radius=0.5)
-s2 = sphere(pos=vector(1.5,-1,1), color=color.black, radius=0.5)
-s3 = sphere(pos=vector(1.5,-1,-1), color=color.black, radius=0.5)
-s4 = sphere(pos=vector(-1.5,-1,-1), color=color.black, radius=0.5)
+car = box(pos=vector(x,0,0), size=vector(4,2,2), color=color.cyan, visible=True)    # carrito
+s1 = sphere(pos=vector(-1.5,-1,1), color=color.black, radius=0.5)                   # rueda 1
+s2 = sphere(pos=vector(1.5,-1,1), color=color.black, radius=0.5)                    # rueda 2
+s3 = sphere(pos=vector(1.5,-1,-1), color=color.black, radius=0.5)                   # rueda 3
+s4 = sphere(pos=vector(-1.5,-1,-1), color=color.black, radius=0.5)                  # rueda 4
 # posición inicial de la bolita
 ball = sphere(pos=vector(x + l*sin(th),-l*cos(th),0), color=color.magenta, radius=0.5, make_trail=True, visible=True)
 # orientación inicial de la varilla
 rod = cylinder(pos=vector(0,0,0), size=vector(4,0.1,0.1), color=color.black)
 rod_ang = rod.rotate(angle=pi-th, axis=vec(0,0,1))
 
-
 while t<10:
     rate(50) # limit animation rate, render scene
-    scene.caption = f't = {round(t,2)} segundos'
+    scene.caption = f'\t Tiempo de simulación \t t = {round(t,2)} segundos'
     # Variables: q[0]=x, q[1]=th,  qdot[0]=xdot, qdot[1]=thdot
 
     # Matrices H, C, G, Bu -----------------------------------------
@@ -95,12 +73,12 @@ while t<10:
     HI= np.linalg.inv(H) #Inversa de H
     T = Bu - np.dot(C,qdot) - G
 
-    # Solución de las ecuaciones --------------------
+    # Solución de las ecuaciones con el método de euler-------------
     qddot = np.dot(HI, T)
     qdot = qdot + qddot*dt
     q = q + qdot*dt
 
-    # Movimiento del sistema -----------------------
+    # Movimiento del sistema ---------------------------------------
     ball.pos = vector(q[0] + l*sin(q[1]),-l*cos(q[1]),0)
     rod.pos=vector(q[0],0,0)
     rod.axis=ball.pos - vector(q[0],0,0)
